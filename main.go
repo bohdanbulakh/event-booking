d82f9546cb99150bc52017e6f0347272f31d74b3
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-02-13 23:19:50 +0200

    added demo endpoint

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	server := gin.Default()
+	loadEnv()
 	port := os.Getenv("PORT")
 
+	server.GET("/events", getEvents)
+
 	server.Run(":" + port)
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"event": "some useful data"})
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.development")
+	if err != nil {
+		log.Fatal("Error loading .env file" +
+			"\n" + err.Error())
+	}
 }
